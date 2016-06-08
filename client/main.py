98a39034cc07f39b756f17a94c8493593340a7f9
aslin1126 <63052533@qq.com>
2016-06-08 22:45:36 +0800

    add relay event

--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 class MotionSensor(Sensor):
     def __init__(self, pin):
-        Sensor.__init__(self,pin)
+		Sensor.__init__(self,pin)
  
     def onStateChange(self, channel):
         print "%s: pin: %d state: %d" % (t.asctime(), channel, self.getState())
@@ -14,14 +14,36 @@ class MotionSensor(Sensor):
         	#send message to client 
 
 
-def boardSensorThread( board, duration):	
-    while True:
-        print "%s: control:%d light:%d temp:%d custom:%d" % (t.asctime(),
-                               board.control(),
-                               board.light(),
-                               board.temperature(),
-                               board.custom())
-        t.sleep(duration)
+def boardSensorThread( relay,board, duration):	
+	lightIsOn = False
+	tempIsOn = False
+	while True:
+		print "%s:  light:%d temp:%d " % (t.asctime(), board.light(),board.temperature())
+		if(board.light()<160):
+			if(lightIsOn == False):
+				print "on light relay"
+				relay.switch(0,1)
+				lightIsOn = True
+		else:
+			if(lightIsOn == True):
+				print "off light relay"
+				relay.switch(0,0)
+				lightIsOn = False
+
+		if(board.temperature()<100):
+			if(tempIsOn == False):
+				if(tempIsOn == False):
+					print "on temp relay"
+					relay.switch(1,1)
+					tempIsOn = True
+		else:
+			if(tempIsOn == True):
+				print "off temp relay"
+				relay.switch(1,0)
+				tempIsOn = False
+
+		t.sleep(duration)
+	
    
 def main():	
 	
@@ -32,10 +54,10 @@ def main():
     relay = Relay()
     #init temperature sensor 
     board = Board()
-    Thread(target=boardSensorThread, args=(board, 3)).start()
+    Thread(target=boardSensorThread, args=(relay,board,3)).start()
 
     while True:
-        t.sleep(1)
+        t.sleep(5)
  
 if __name__ == "__main__":
     main()
