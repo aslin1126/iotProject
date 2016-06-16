547ca47186e2244e0c88b65c706e0d923ad2fe04
aslin1126 <63052533@qq.com>
2016-06-16 21:59:34 +0800

    update

--- iotserver/iotserver/views.py
+++ iotserver/iotserver/views.py
@@ -25,11 +25,11 @@ def listrecords(request):
         connection = sqlite3.connect('db.sqlite3')
         with connection:
             cursor1 = connection.cursor()
-            cursor1.execute("select * from record order by 1 desc limit 1")
+            cursor1.execute("select * from record order by record_timestamp desc limit 1")
             data['records1'] = cursor1.fetchall()
 
             cursor2 = connection.cursor()
-            cursor2.execute("select * from record order by 1 desc limit 10")
+            cursor2.execute("select * from record order by record_timestamp desc limit 25")
             data['records2'] = cursor2.fetchall()
             print  data['records1'][0]
             print  data['records2'][0]
@@ -47,37 +47,38 @@ def saverecord(request,data):
     #http://192.168.1.118:8080/views/data   -- this is the format 
     print "new data is " + data
     input =str(data)
-    record = t.asctime() +"----"
+    record_detail = ""
+    record_timestamp = t.asctime()
     if "motion=1" in input:
-        record += " alert that someone is move around this item" 
+        record_detail += " alert that someone is move around this item" 
     elif "," in input:
         light, temp = input.split(',')
         if "on" in light:
-            record += "light is too dark, the light is on "
+            record_detail += "light is too dark, the light is on "
         elif "off" in light:
-            record += "light is too bright, the light is off "
+            record_detail += "light is too bright, the light is off "
         else:
-            record += ""+light
+            record_detail += ""+light
 
         if "on" in temp:
-            record += ",tempature is too cold, the heater is on ."
+            record_detail += ",tempature is too cold, the heater is on ."
         elif "off" in temp:
-            record += ",tempature is too hot, the heater is off ."
+            record_detail += ",tempature is too hot, the heater is off ."
         else:
-            record += ","+temp	
+            record_detail += ","+temp	
 
         
-    print "new record is " + record                    
+    print "new record is " + record_detail + " at time of " + record_timestamp
     connection = None
     try:
         connection = sqlite3.connect('db.sqlite3')
         with connection:
             cursor = connection.cursor()    
-            cursor.execute("INSERT INTO record VALUES (?)", [record])
+            cursor.execute("INSERT INTO record(detail) VALUES (?)", [record_detail])
     except sqlite3.Error, e:
         print "Error %s:" % e.args[0]
         sys.exit(1)
     finally:
         if connection:
             connection.close()
-    return HttpResponse("New data '"+record+"' is saved")
+    return HttpResponse("New data '"+record_detail+"' is saved")
